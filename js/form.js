a41b84478003337cd8e2ec5b2ae07533e37ddb23
Mie-Nel <mielikki@mail.ru>
2018-06-27 21:08:16 +0300

    модуляция

---
+++ js/form.js
@@ -0,0 +1,171 @@
+'use strict';
+
+
+(function () {
+  var form = document.querySelector('.ad-form');
+  var inputTitle = form.querySelector('#title');
+  var inputPrice = form.querySelector('#price');
+  var selectType = form.querySelector('#type');
+  var selectTimeIn = form.querySelector('#timein');
+  var selectTimeOut = form.querySelector('#timeout');
+  var fieldsets = form.querySelectorAll('fieldset');
+
+  var disableFormElement = function () {
+    for (var i = 0; i < fieldsets.length; i++) {
+      fieldsets[i].setAttribute('disabled', 'disabled');
+    }
+    return fieldsets;
+  };
+  disableFormElement();
+
+  var enableFormElement = function () {
+    for (var j = 0; j < fieldsets.length; j++) {
+      fieldsets[j].removeAttribute('disabled');
+    }
+    form.classList.remove('ad-form--disabled');
+  };
+
+  var onInputTitleInvalid = function () {
+    if (inputTitle.validity.tooShort) {
+      inputTitle.setCustomValidity('Имя должно состоять минимум из 30 символов');
+    } else if (inputTitle.validity.tooLong) {
+      inputTitle.setCustomValidity('Имя не должно превышать 100 символов');
+    } else if (inputTitle.validity.valueMissing) {
+      inputTitle.setCustomValidity('Обязательное поле');
+    } else {
+      inputTitle.setCustomValidity('');
+    }
+  };
+
+  inputTitle.addEventListener('invalid', onInputTitleInvalid);
+
+  var onInputPriceInvalid = function () {
+    if (inputPrice.validity.rangeOverflow) {
+      inputPrice.setCustomValidity('Цена не должна превышать 1000000');
+    } else if (inputPrice.validity.valueMissing) {
+      inputPrice.setCustomValidity('Обязательное поле');
+    } else {
+      inputPrice.setCustomValidity('');
+    }
+  };
+  inputPrice.addEventListener('invalid', onInputPriceInvalid);
+
+
+  var minPriceBungalo = 0;
+  var minPriceFlat = 1000;
+  var minPriceHouse = 5000;
+  var minPricePalace = 10000;
+
+  inputPrice.setAttribute('placeholder', minPriceFlat);
+  inputPrice.setAttribute('min', minPriceFlat);
+
+  var onSelectChange = function () {
+    if (selectType.value === 'bungalo') {
+      inputPrice.min = minPriceBungalo;
+      inputPrice.placeholder = minPriceBungalo;
+    } else if (selectType.value === 'flat') {
+      inputPrice.min = minPriceFlat;
+      inputPrice.placeholder = minPriceFlat;
+    } else if (selectType.value === 'house') {
+      inputPrice.min = minPriceHouse;
+      inputPrice.placeholder = minPriceHouse;
+    } else if (selectType.value === 'palace') {
+      inputPrice.min = minPricePalace;
+      inputPrice.placeholder = minPricePalace;
+    }
+  };
+  selectType.addEventListener('change', onSelectChange);
+
+
+  var checkTimeInOut = function () {
+    switch (selectTimeIn.value) {
+      case '12:00':
+        selectTimeOut.value = selectTimeIn.value;
+        break;
+
+      case '13:00':
+        selectTimeOut.value = selectTimeIn.value;
+        break;
+
+      case '14:00':
+        selectTimeOut.value = selectTimeIn.value;
+        break;
+    }
+  };
+  selectTimeIn.addEventListener('change', checkTimeInOut);
+
+  var checkTimeOutIn = function () {
+    switch (selectTimeOut.value) {
+      case '12:00':
+        selectTimeIn.value = selectTimeOut.value;
+        break;
+
+      case '13:00':
+        selectTimeIn.value = selectTimeOut.value;
+        break;
+
+      case '14:00':
+        selectTimeIn.value = selectTimeOut.value;
+        break;
+    }
+  };
+  selectTimeOut.addEventListener('change', checkTimeOutIn);
+
+
+  var numberOfRooms = form.elements.rooms;
+  var numberOfBedrooms = form.elements.capacity;
+
+
+  var setDisabledCapacity = function () {
+    numberOfBedrooms.options[0].disabled = true;
+    numberOfBedrooms.options[1].disabled = true;
+    numberOfBedrooms.options[3].disabled = true;
+    numberOfBedrooms.options[2].selected = true;
+  };
+
+
+  var setAllOptions = function () {
+    for (var j = 0; j < numberOfBedrooms.options.length; j++) {
+      if (numberOfBedrooms.options[j].hasAttribute('disabled', 'disabled')) {
+        numberOfBedrooms.options[j].removeAttribute('disabled', false);
+      }
+      if (numberOfBedrooms.options[j].hasAttribute('selected', true)) {
+        numberOfBedrooms.options[j].removeAttribute('selected');
+      }
+    }
+  };
+
+  var disabledElements = function () {
+    setAllOptions();
+    switch (numberOfRooms.value) {
+      case '1':
+        numberOfBedrooms.options[0].disabled = true;
+        numberOfBedrooms.options[1].disabled = true;
+        numberOfBedrooms.options[3].disabled = true;
+        numberOfBedrooms.options[2].selected = true;
+        break;
+      case '2':
+        numberOfBedrooms.options[0].disabled = true;
+        numberOfBedrooms.options[3].disabled = true;
+        numberOfBedrooms.options[2].selected = true;
+        break;
+      case '3':
+        numberOfBedrooms.options[3].disabled = true;
+        numberOfBedrooms.options[2].selected = true;
+        break;
+      case '100':
+        numberOfBedrooms.options[0].disabled = true;
+        numberOfBedrooms.options[1].disabled = true;
+        numberOfBedrooms.options[2].disabled = true;
+        numberOfBedrooms.options[3].selected = true;
+    }
+  };
+  numberOfRooms.addEventListener('change', function () {
+    disabledElements();
+  });
+
+  window.formElements = {
+    enableFormElement: enableFormElement,
+    setDisabledCapacity: setDisabledCapacity,
+  };
+})();
---
+++ js/pin-main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+(function () {
+  var map = document.querySelector('.map');
+  var pinMain = document.querySelector('.map__pin--main');
+
+
+  function pinMainMouseup() {
+    map.classList.remove('map--faded');
+    window.formElements.enableFormElement();
+    window.formElements.setDisabledCapacity();
+    window.pins.renderPins(window.pins.adverts);
+  }
+
+
+  pinMain.addEventListener('mouseup', function () {
+    pinMainMouseup();
+  });
+
+  var PIN_WIDTH = 65;
+  var inputAddress = document.querySelector('#address');
+
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      inputAddress.value = startCoords.x + ' ' + startCoords.y;
+
+      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+
+      var MIN_LEFT = 0;
+      var MAX_RIGHT = 1200 - PIN_WIDTH;
+      var MIN_TOP = 130;
+      var MAX_BOTTOM = 630;
+
+      if (startCoords.x < MIN_LEFT) {
+        pinMain.style.left = MIN_LEFT + PIN_WIDTH + 'px';
+      }
+      if (startCoords.x > MAX_RIGHT) {
+        pinMain.style.left = MAX_RIGHT + 'px';
+      }
+      if (startCoords.y < MIN_TOP) {
+        pinMain.style.top = MIN_TOP + 'px';
+      }
+      if (startCoords.y > MAX_BOTTOM) {
+        pinMain.style.top = MAX_BOTTOM + 'px';
+      }
+    };
+
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
